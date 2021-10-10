fcf99147563fc76626385fcd9860ce59f573403d
bellwest <bellwest93@gmail.com>
2021-10-10 15:55:34 +0900

    add quotes and backgrounds

---
+++ js/background.js
@@ -0,0 +1,6 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const image = document.createElement("img");
+image.src = `img/${chosenImage}`;
+document.body.appendChild(image); 
